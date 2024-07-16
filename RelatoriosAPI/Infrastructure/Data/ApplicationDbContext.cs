0afdfcafacef5a3b506b8550c599866e2b9aae14
Stanislaw Cruz <stanycruz@outlook.com>
2024-07-16 20:44:11 -0300

    feat: add ApplicationDbContext

--- RelatoriosAPI/Infrastructure/Data/ApplicationDbContext.cs
+++ RelatoriosAPI/Infrastructure/Data/ApplicationDbContext.cs
@@ -1,12 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RelatoriosAPI.Domain.Entities;
 
 namespace RelatoriosAPI.Infrastructure.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
-        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
