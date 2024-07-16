1907f1d21425f6bb77d4115169f7fb480174454b
Stanislaw Cruz <stanycruz@outlook.com>
2024-07-16 20:23:54 -0300

    refactor: add directory structure

---
+++ RelatoriosAPI/Domain/Entities/Product.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RelatoriosAPI.Domain.Entities
+{
+    public class Product
+    {
+        
+    }
+}
