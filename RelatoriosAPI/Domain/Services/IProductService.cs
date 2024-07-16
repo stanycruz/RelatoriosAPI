275e79e3e7225c5b16d819d4fbbf6c0bb89dd5b0
Stanislaw Cruz <stanycruz@outlook.com>
2024-07-16 20:39:48 -0300

    feat: add product domain service interface

--- RelatoriosAPI/Domain/Services/IProductService.cs
+++ RelatoriosAPI/Domain/Services/IProductService.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using RelatoriosAPI.Domain.Entities;
 
 namespace RelatoriosAPI.Domain.Services
 {
     public interface IProductService
     {
-        
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<Product> GetProductByIdAsync(int productId);
     }
 }
