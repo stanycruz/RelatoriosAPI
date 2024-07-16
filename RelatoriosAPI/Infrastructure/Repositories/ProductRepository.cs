4205fbbd1366b2ab2074641eb88c0d884cf0dac3
Stanislaw Cruz <stanycruz@outlook.com>
2024-07-16 20:47:48 -0300

    feat: implement repositories

--- RelatoriosAPI/Infrastructure/Repositories/ProductRepository.cs
+++ RelatoriosAPI/Infrastructure/Repositories/ProductRepository.cs
@@ -1,12 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RelatoriosAPI.Domain.Entities;
+using RelatoriosAPI.Domain.Interfaces;
+using RelatoriosAPI.Infrastructure.Data;
 
 namespace RelatoriosAPI.Infrastructure.Repositories
 {
-    public class ProductRepository
+    public class ProductRepository : IProductRepository
     {
-        
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetProductByIdAsync(int productId)
+        {
+            return await _context.Products.FindAsync(productId);
+        }
     }
 }
