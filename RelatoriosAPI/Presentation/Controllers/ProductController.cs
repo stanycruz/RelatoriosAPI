8f0cfa1b49c94fce88d036baaee4a852d6352081
Stanislaw Cruz <stanycruz@outlook.com>
2024-07-16 21:30:09 -0300

    feat: add product controller

--- RelatoriosAPI/Presentation/Controllers/ProductController.cs
+++ RelatoriosAPI/Presentation/Controllers/ProductController.cs
@@ -1,8 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RelatoriosAPI.Domain.Entities;
+using RelatoriosAPI.Domain.Services;
 
 namespace RelatoriosAPI.Presentation.Controllers
 {
@@ -10,6 +8,29 @@ namespace RelatoriosAPI.Presentation.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-        
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetAll()
+        {
+            var products = await _productService.GetAllProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetById(int id)
+        {
+            var product = await _productService.GetProductByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
     }
 }
