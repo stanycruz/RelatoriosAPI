032edbaf096bc9c8763412ff7e4a6a828a06da60
Stanislaw Cruz <stanycruz@outlook.com>
2024-07-16 21:26:54 -0300

    feat: add ApplicationDbContext configuration

--- RelatoriosAPI/Program.cs
+++ RelatoriosAPI/Program.cs
@@ -1,4 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using RelatoriosAPI.Application.Services;
+using RelatoriosAPI.Domain.Interfaces;
+using RelatoriosAPI.Domain.Services;
+using RelatoriosAPI.Infrastructure.Data;
+using RelatoriosAPI.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
