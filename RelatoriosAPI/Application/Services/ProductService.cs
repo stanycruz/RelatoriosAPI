58bf87cc1516ebb0e8baad739acd04e7cd64fd4e
Stanislaw Cruz <stanycruz@outlook.com>
2024-07-16 21:21:45 -0300

    feat: implement app services

--- RelatoriosAPI/Application/Services/ProductService.cs
+++ RelatoriosAPI/Application/Services/ProductService.cs
@@ -1,12 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using RelatoriosAPI.Domain.Entities;
+using RelatoriosAPI.Domain.Interfaces;
+using RelatoriosAPI.Domain.Services;
 
 namespace RelatoriosAPI.Application.Services
 {
-    public class ProductService
+    public class ProductService : IProductService
     {
-        
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            return await _productRepository.GetAllProductsAsync();
+        }
+
+        public async Task<Product> GetProductByIdAsync(int productId)
+        {
+            return await _productRepository.GetProductByIdAsync(productId);
+        }
     }
 }
